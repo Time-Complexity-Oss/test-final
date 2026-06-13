f73db0f81d728041d99d3e7195ce8f5ef4f2bef5
Pratik Debnath <pratikdebnath978@gmail.com>
2026-06-13 16:00:19 +0530

    Fixed duplicate header

--- packages/esm-appointments-app/src/calendar/monthly/monthly-calendar-view.component.tsx
+++ packages/esm-appointments-app/src/calendar/monthly/monthly-calendar-view.component.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import isBetween from 'dayjs/plugin/isBetween';
 import dayjs, { type Dayjs } from 'dayjs';
 import { type DailyAppointmentsCountByService } from '../../types';
@@ -13,7 +13,6 @@ interface MonthlyCalendarViewProps {
   events: Array<DailyAppointmentsCountByService>;
   calendarSelectedDate: Dayjs;
   calKey?: string;
-  setCalendarSelectedDate: React.Dispatch<React.SetStateAction<Dayjs>>;
   onSelectDate?: (isoDate: string) => void;
 }
 
@@ -21,24 +20,11 @@ const MonthlyCalendarView: React.FC<MonthlyCalendarViewProps> = ({
   events,
   calendarSelectedDate,
   calKey = 'gregory',
-  setCalendarSelectedDate,
   onSelectDate,
 }) => {
-  const handlePrev = useCallback(
-    () => setCalendarSelectedDate((d) => d.subtract(1, 'month')),
-    [setCalendarSelectedDate],
-  );
-
-  const handleNext = useCallback(() => setCalendarSelectedDate((d) => d.add(1, 'month')), [setCalendarSelectedDate]);
-
   return (
     <div className={styles.calendarViewContainer}>
-      <MonthlyHeader
-        calendarSelectedDate={calendarSelectedDate}
-        calKey={calKey}
-        onSelectPrevMonth={handlePrev}
-        onSelectNextMonth={handleNext}
-      />
+      <MonthlyHeader calKey={calKey} />
       <div className={styles.wrapper}>
         <div className={styles.monthlyCalendar}>
           {monthDays(calendarSelectedDate).map((dateTime, i) => (
